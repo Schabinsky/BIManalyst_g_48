088f111ec720bd8e96c585f94cc69133a4492b2e
NKPoulsen <132125951+NKPoulsen@users.noreply.github.com>
2025-09-16 09:59:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,24 +18,35 @@ except OSError:
 
 # Your script goes here
 spaces = model.by_type("IfcSpace")
-meeting_rooms = []
+meeting_room = []
+meeting_room_no = []
+room_no = -1
 
 for space in spaces:
     # print('{} - {}'.format(space.Name, space.LongName))
     if space.LongName == 'Meeting room':
-        meeting_rooms.append(int(space.Name))
+        meeting_room_no.append(room_no)
+        meeting_room.append(int(space.Name))
+        room_no = room_no + 1
     else:
+        room_no = room_no + 1
         continue
-print(meeting_rooms)
+print(meeting_room)
 
 # PROBLEM: index bliver forkert i koden herunder fordi spaces i spaces listen starter ved 65 og ikke 0. 
 # Find anden måde at hente de rigtige rooms fra den lange liste spaces.
+# Found a way ;-)
+meeting_areas = []
 
-for room in meeting_rooms:
+for room in meeting_room_no:
     # print(spaces[room+1])
     qtos = ifcopenshell.util.element.get_psets(spaces[room+1], qtos_only=True)
     print('The area for Meeting room: ' + str(spaces[room+1].Name) + ' is ' + str(qtos['Qto_SpaceBaseQuantities']['NetFloorArea']))
+    meeting_areas.append(str(qtos['Qto_SpaceBaseQuantities']['NetFloorArea']))
 
 # Hvor mange spaces er der?
+
 # Hvilke af disse spaces er har label 'Meeting Room'?
+print('A total of' + str(len(meeting_room)) + 'meeting rooms are present in the model')
+
 # Hvor store er disse spaces?
